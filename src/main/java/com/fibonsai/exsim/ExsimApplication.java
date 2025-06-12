ee8d9b5530b5e7f7cadb9f7ba2ae1b9a8c9384de
tuxmonteiro <me@tuxmonteiro.dev>
2025-06-12 14:59:16 +0200

    add initial src autogenerated

---
+++ src/main/java/com/fibonsai/exsim/ExsimApplication.java
@@ -0,0 +1,13 @@
+package com.fibonsai.exsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExsimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fibonsai/exsim/ExsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fibonsai.exsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
