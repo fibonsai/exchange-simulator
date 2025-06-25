eb1e27c45881f601a1aa832aeda07ec51f342ebc
tuxmonteiro <me@tuxmonteiro.dev>
2025-06-25 17:41:45 +0200

    Rename ProtocolStandard to Protocol and change type to enum

---
+++ src/main/java/com/fibonsai/exsim/dto/Protocol.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2025 fibonsai.com
+ *  All rights reserved.
+ *
+ *  This source is subject to the Apache License, Version 2.0.
+ *  Please see the LICENSE file for more information.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.fibonsai.exsim.dto;
+
+@SuppressWarnings("SpellCheckingInspection")
+public enum Protocol {
+    APTOSFA,
+    ARC20,
+    ASA,
+    ASSET_PALLET,
+    BEP2,
+    BEP20,
+    BRC20,
+    CRC20,
+    CW20,
+    DOG20,
+    EOSIO,
+    ERC1155,
+    ERC20,
+    ERC223,
+    ERC4626,
+    ERC721,
+    ERC777,
+    ESDT,
+    ESDT_NFT,
+    ESDT_SFT,
+    FA12,
+    FA2,
+    FFT,
+    HRC20,
+    HTS,
+    IBC,
+    ICRC1,
+    IOTXXRC20,
+    IRC16,
+    IRC2,
+    IRC3,
+    JETTON,
+    KIP20,
+    KRC20,
+    NEP11,
+    NEP141,
+    NEP17,
+    NEP5,
+    OEP4,
+    QRC20,
+    RUNE,
+    SAS,
+    SPL,
+    SUI20,
+    TRC10,
+    TRC20,
+    TRC721,
+    VIP180,
+    WAVES,
+    XRC20,
+    XRP
+}
