619a4c92b0e44cf9a81fb2d07a8ee76f935920e4
tuxmonteiro <me@tuxmonteiro.dev>
2025-06-25 17:42:07 +0200

    Update JacksonMapperConfigurator

--- src/main/java/com/fibonsai/exsim/config/JacksonMapperConfigurator.java
+++ src/main/java/com/fibonsai/exsim/config/JacksonMapperConfigurator.java
@@ -14,8 +14,14 @@
 
 package com.fibonsai.exsim.config;
 
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fibonsai.exsim.dto.AssetCustodian;
+import com.fibonsai.exsim.dto.Protocol;
+import com.fibonsai.exsim.dto.deser.AssetCustodianDeserializer;
+import com.fibonsai.exsim.dto.deser.ProtocolDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,7 +30,14 @@ public class JacksonMapperConfigurator {
 
     @Bean
     public ObjectMapper objectMapper() {
+        Version version = new Version(1, 0 , 0, null, null, null);
+        SimpleModule assetProtocolModule = new SimpleModule("ProtocolDeserializer", version);
+        SimpleModule assetCustodianModule = new SimpleModule("AssetCustodianDeserializer", version);
+        assetProtocolModule.addDeserializer(Protocol.class, new ProtocolDeserializer());
+        assetCustodianModule.addDeserializer(AssetCustodian.class, new AssetCustodianDeserializer());
         return new ObjectMapper()
+                .registerModule(assetProtocolModule)
+                .registerModule(assetCustodianModule)
                 .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
