3c7583096f7b478d560e56486d0ef07e809124a6
tuxmonteiro <me@tuxmonteiro.dev>
2025-06-29 14:46:55 +0200

    Improve tests

---
+++ src/test/java/com/fibonsai/exsim/services/AccountServiceTest.java
@@ -0,0 +1,54 @@
+
+/*
+ *  Copyright (c) 2025 fibonsai.com
+ *  All rights reserved.
+ *
+ *  This source is subject to the Apache License, Version 2.0.
+ *  Please see the LICENSE file for more information.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.fibonsai.exsim.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@SpringBootTest
+class AccountServiceTest {
+
+    @Autowired
+    private WalletService walletService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Test
+    void testAddAccount() {
+        String accountStr = "testAccount";
+
+        StepVerifier.create(accountService.addAccount(accountStr))
+                .expectNext(accountStr)
+                .verifyComplete();
+
+        assertNotNull(walletService.getDefaultWallet(accountStr));
+    }
+
+    @Test
+    void testAddAccountWithExistingAccount() {
+        String accountStr = "existingAccount";
+
+        accountService.addAccount(accountStr).subscribe(log::info);
+        accountService.addAccount(accountStr).subscribe(log::info);
+    }
+}
---
+++ src/test/java/com/fibonsai/exsim/services/AssetServiceTest.java
@@ -0,0 +1,78 @@
+
+/*
+ *  Copyright (c) 2025 fibonsai.com
+ *  All rights reserved.
+ *
+ *  This source is subject to the Apache License, Version 2.0.
+ *  Please see the LICENSE file for more information.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.fibonsai.exsim.services;
+
+import com.fibonsai.exsim.config.JacksonMapperConfigurator;
+import com.fibonsai.exsim.dto.asset.Asset;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Currency;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = {AssetService.class, JacksonMapperConfigurator.class})
+class AssetServiceTest {
+
+    @Autowired
+    private AssetService assetService;
+
+    @BeforeEach
+    void setUp() {
+        assetService.init();
+    }
+
+    @Test
+    void testDefaultAsset() {
+        Asset defaultAsset = assetService.defaultAsset();
+        assertEquals(AssetService.DEFAULT_ASSET, defaultAsset.symbol());
+    }
+
+    @Test
+    void testAddCurrency() {
+        Currency currency = Currency.getInstance("EUR");
+        assetService.add(currency);
+        assertTrue(assetService.assets().containsKey("EUR"));
+    }
+
+    @Test
+    void testAddAsset() {
+        Asset asset = Asset.builder().symbol("TEST").build();
+        assetService.add(asset);
+        assertTrue(assetService.assets().containsKey("TEST"));
+    }
+
+    @Test
+    void testLoadFiatAssets() {
+        assetService.loadFiatAssets();
+        assertTrue(assetService.assets().size() > 0);
+    }
+
+    @Test
+    void testLoadFromFile() {
+        assetService.loadFromFile();
+        assertTrue(assetService.assets().size() > 0);
+    }
+
+    @Test
+    void testAssets() {
+        Map<String, Asset> assets = assetService.assets();
+        assertThrows(UnsupportedOperationException.class, () -> assets.put("test", Asset.builder().build()));
+    }
+}
